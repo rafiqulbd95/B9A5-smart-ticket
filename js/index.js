1207abe0f3f606345799435128170fea623363e6
Rafiqul Islam <rafiqulbd95@gmail.com>
2024-02-19 14:07:07 +0600

    js done

--- js/index.js
+++ js/index.js
@@ -1,23 +1,22 @@
 const seatBtn = document.getElementsByClassName("seatBtn");
 
 for (const btn of seatBtn) {
+
+
   btn.addEventListener("click", function () {
     btn.style.backgroundColor = "#1DD100";
 
-    const currentSeat = document.getElementById("total-seat").innerText;
-    const convertSeat = parseInt(currentSeat);
 
-    const updateSeat = convertSeat - 1;
+
+
+    const updateSeat = innerText("total-seat") - 1;
     document.getElementById("total-seat").innerText = updateSeat;
 
-    const currentSeats = document.getElementById("update-seat").innerText;
-    const convertSeatNum = parseInt(currentSeats);
 
-    const updateSeatNum = convertSeatNum + 1;
+    const updateSeatNum = innerText("update-seat") + 1;
     document.getElementById("update-seat").innerText = updateSeatNum;
 
-    const price = document.getElementById("price").innerText;
-    const convertedPrice = parseInt(price);
+    innerText("price");
 
     const selectSeat = document.getElementById("select-seat");
     const li = document.createElement("li");
@@ -27,36 +26,50 @@ for (const btn of seatBtn) {
     const p3 = document.createElement("p");
     p.innerText = btn.innerText;
     p2.innerText = "Economoy";
-    p3.innerText = convertedPrice;
+    p3.innerText = innerText("price");
     li.appendChild(p);
     li.appendChild(p2);
     li.appendChild(p3);
     selectSeat.appendChild(li);
 
     // totalPrice
-    const totalPrice = document.getElementById("total-price").innerText;
-    const convertPrice = parseInt(totalPrice);
-    const totalSeatPrice = (document.getElementById("total-price").innerText =
-      convertPrice + convertedPrice);
 
-    // grandTotalPrice
-    const grandTotalPrice = document.getElementById("grand-total").innerText;
-    const convertGrandPrice = parseInt(grandTotalPrice);
+    innerText("total-price");
+    document.getElementById("total-price").innerText =
+      innerText("total-price") + innerText("price");
+
+
+    innerText("grand-total");
     document.getElementById("grand-total").innerText =
-      convertGrandPrice + convertedPrice;
+      innerText("grand-total") + innerText("price");
 
     //   --------------
-    
+
   });
 }
 
-const applyBtn = document.getElementById("apply-btn");
-applyBtn.addEventListener("click", function () {
-    const inputValue = document.getElementById("input_value").value;
 
-    if (inputValue.toUpperCase() === "NEW15") {
-      document.getElementById("coupon-area").classList.add("hidden");
-    } else if(inputValue.toUpperCase() === "COUPLE 20"){
-        document.getElementById("coupon-area").classList.add("hidden");
-    }
+const applyBtn = document.getElementById('apply-btn');
+applyBtn.addEventListener('click', function () {
+  const inputValue = document.getElementById("input_value").value;
+
+  if (inputValue.toUpperCase() === "NEW15") {
+    const totalPrice = parseFloat(document.getElementById("total-price").innerText);
+    const discountedPrice = totalPrice * 0.15;
+    document.getElementById("grand-total").innerText =  totalPrice - discountedPrice;
+    document.getElementById("coupon-area").classList.add("hidden");
+
+  } else if (inputValue.toUpperCase() === "COUPLE 20") {
+    const totalPrice = parseFloat(document.getElementById("total-price").innerText);
+    const discountedPrice = totalPrice * 0.20;
+    document.getElementById("grand-total").innerText =  totalPrice - discountedPrice;
+    document.getElementById("coupon-area").classList.add("hidden");
+  }
 });
+
+
+function innerText(Id) {
+  const currentSeats = document.getElementById(Id).innerText;
+  const convertSeatNum = parseInt(currentSeats);
+  return convertSeatNum;
+}
